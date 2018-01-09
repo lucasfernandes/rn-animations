cd36fff3c16b40cd0594e9aedd14cfbe6f2f8b84
Lucas Silveira <lucas.fernandes.silveira@gmail.com>
2018-01-08 23:18:38 -0200

    interpolating values to fade the ball

--- src/pages/timing/index.js
+++ src/pages/timing/index.js
@@ -10,69 +10,14 @@ import styles from './styles';
 export default class Timing extends Component {
   state = {
     ballY: new Animated.Value(0),
-    ballX: new Animated.Value(0),
   };
 
-  // timingY = () =>  Animated.timing(this.state.ballY, {
-  //   toValue: 200,
-  //   duration: 500,
-  // });
-  
-  // timingX = () => Animated.timing(this.state.ballX, {
-  //   toValue: 200,
-  //   duration: 500,
-  // });
-  
-  // Animated.parallel - executes in parallel
-  // Animated.sequence - executes in sequence (one after another)
-  // Animated.sequence([
-  //   this.timingY(),
-  //   Animated.delay(1000),
-  //   this.timingX(),
-  // ]).start();
-
-
-  // executes each animation after a specifica time (paralel after this time)
-  // Animated.stagger(300, [
-  //   this.timingY(),
-  //   Animated.delay(1000),
-  //   this.timingX(),
-  // ]).start();
-
   componentDidMount() {
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(this.state.ballY, {
-          toValue: 200,
-          duration: 500,
-        }),
-
-        Animated.delay(500),
-
-        Animated.timing(this.state.ballX, {
-          toValue: 200,
-          duration: 500,
-        }),
-
-        Animated.delay(500),
-
-        Animated.timing(this.state.ballY, {
-          toValue: 0,
-          duration: 500,
-        }),
-
-        Animated.delay(500),
-
-        Animated.timing(this.state.ballX, {
-          toValue: 0,
-          duration: 500,
-        }),
-
-        Animated.delay(500),
-      ]), {
-        iterations: 2,
-      },
-     ).start();
+    
+    Animated.timing(this.state.ballY, {
+      toValue: 500,
+      duration: 1000,
+    }).start();
   }
 
   render() {
@@ -82,7 +27,11 @@ export default class Timing extends Component {
           styles.ball,
           { 
             top: this.state.ballY,
-            left: this.state.ballX,
+            opacity: this.state.ballY.interpolate({
+              inputRange: [0, 300],
+              outputRange: [1, 0.2],
+              extrapolate: 'clamp',
+            }),
           }
         ]} />
       </View>
